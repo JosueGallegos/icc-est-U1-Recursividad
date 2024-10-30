35cc718727c9e6198a5eef9b69767f6b063f2e65
JosueGallegos <jgallegoss@est.ups.edu.ec>
2024-10-30 12:35:14 -0500

    Fibonacci

--- src/RenombrarDirectorios.java
+++ src/RenombrarDirectorios.java
@@ -6,11 +6,30 @@ public class RenombrarDirectorios {
         if(!directorio.exists() || !directorio.isDirectory()){
             System.out.println("La ruta path no es un directorio valido. ");
             return;
-
         }
         //aca vamos a cambiar los nombres 
-        System.out.println("La ruta path es valida. ");
-        
+        //System.out.println("La ruta path es valida. ");
+        renombrarDirectoriosInternos(directorio);
+
+
+    }
+    public void renombrarDirectoriosInternos(File directorio){
+        //Listar todos los archivos y carpetas hijas
+        File[] directoriosArchivosInternos = directorio.listFiles();
+        if(directoriosArchivosInternos == null){
+            return;
+        }
+        for (File directorioArchivo : directoriosArchivosInternos) {
+            if (directorioArchivo.isDirectory()) {// Si es carpeta cambia el nombre 
+                
+                String nuevoNombre = "Nuevos" + " NombreOriginal";
+                File nuevoDirectorio = new File(directorioArchivo.getPath(),nuevoNombre);
+                if (directorioArchivo.renameTo(nuevoDirectorio)) {
+                    renombrarDirectoriosInternos(nuevoDirectorio);
+                }
+                
+            }
+        }
 
     }
     
